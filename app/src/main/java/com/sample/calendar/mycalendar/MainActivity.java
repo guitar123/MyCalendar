ced4a9110774efba453166be29fa10f3d7363302
gasol <13838470717@163.com>
2017-06-11 22:11:40 +0800

    修改

--- app/src/main/java/com/sample/calendar/mycalendar/MainActivity.java
+++ app/src/main/java/com/sample/calendar/mycalendar/MainActivity.java
@@ -1,13 +1,27 @@
 package com.sample.calendar.mycalendar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity  {
+
+    private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView() {
+        et = (EditText) findViewById(R.id.et);
+
     }
 }
